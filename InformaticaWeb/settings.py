9f6a54588c29bfb51b3570198e640b3bf9c15527
luisiotec <opinador2012@gmail.com>
2024-06-18 18:48:12 -0600

    Mensaje descriptivo del commit

--- InformaticaWeb/settings.py
+++ InformaticaWeb/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-r8!5cm04rhxljj^f&e96!p7=lq*jy_1ulia#qrdbze$=xln_io"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
